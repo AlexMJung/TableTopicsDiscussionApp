ccd619b597a3629ab2def94126dec0327040f06b
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-04-25 13:28:33 -0500

    builds input objects and angular logic

--- client/scripts/controllers/userController.js
+++ client/scripts/controllers/userController.js
@@ -1,4 +1,28 @@
 myApp.controller('UserController', ['$scope', '$http', '$location', 'UserService', function($scope, $http, $location, UserService) {
   $scope.userObject = UserService.userObject;
   $scope.logout = UserService.logout;
+
+  var sessionObject = {};
+  var sessionName = "Firsties";
+  var theme = "Interview Questions";
+  var questionsArray = ["Tell me about a time you went above and beyond.","What is your biggest fear?","How many kittens are too many?"];
+
+  function addQuestion(){
+
+
+  }//ends addQuestion
+
+  sessionObject.sessionName = sessionName;
+  sessionObject.theme = theme;
+  sessionObject.questionsArray = questionsArray;
+
+  console.log("sessionObject", sessionObject);
+
+  $scope.newSession;
+  $scope.newTheme;
+  $scope.newQuestion;
+  $scope.addQuestion = addQuestion;
+
+  $scope.sessionObject = sessionObject;
+
 }]);
