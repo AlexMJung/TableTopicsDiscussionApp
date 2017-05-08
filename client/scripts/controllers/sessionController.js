168ed70ce093880972b104c57d2754a4c4c0a7c6
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-05-07 19:56:46 -0500

    adds true to reload method

--- client/scripts/controllers/sessionController.js
+++ client/scripts/controllers/sessionController.js
@@ -91,7 +91,7 @@ myApp.controller('SessionController', ['$scope', '$interval', '$location', 'User
     $scope.currentThemeObject.questionsArray = [];
     currentSessionObject.participantsArray =[];
     $location.path("/chooseTheme");
-    location.reload();
+    location.reload(true);
   }//ends logoutAndUpdate
 
   }]);//ends sessionIntroController
